08bc9d1977e3376df1ad7fef50a2f04d90340014
Naoto_ntp <sonimario101@gmail.com>
2024-08-02 02:16:36 -0400

    Update syntax.h

--- syntax/naoto/syntax.h
+++ syntax/naoto/syntax.h
@@ -1,12 +1,16 @@
 /* syntax.h  syntax header file for vasm */
-/* (c) in 2015,2017 by Frank Wille */
+/* (c) in 2024 by 'Naoto' */
 
 /* macros to recognize identifiers */
 int isidchar(char);
-#define ISIDSTART(x) ((x)=='.'||(x)=='_'||isalpha((unsigned char)(x)))
+#define ISIDSTART(x) ((x)=='.'||(x)=='@'||(x)=='_'||isalpha((unsigned char)(x)))
 #define ISIDCHAR(x) isidchar(x)
-#define ISBADID(p,l) ((l)==1&&(*(p)=='.'||*(p)=='_'))
+#define ISBADID(p,l) ((l)==1&&(*(p)=='.'||*(p)=='@'||*(p)=='_'))
 #define ISEOL(p) (*(p)=='\0' || *(p)==commentchar)
+#ifdef VASM_CPU_M68K
+char *chkidend(char *,char *);
+#define CHKIDEND(s,e) chkidend((s),(e))
+#endif
 
 /* result of a boolean operation */
 #define BOOLEAN(x) -(x)
